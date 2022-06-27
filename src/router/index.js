b70bbf60d3998548192eb2ebf246ea89ab9e090d
stephen-sanjaya <stephen.sjy.dev@gmail.com>
2022-06-27 15:16:55 +0700

    fix router issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import  {Switch, Route, Routes} from 'react-router';
+import {Route, Routes} from 'react-router-dom';
 
-import Routes from '../pages/NoMatch'
+import NoMatch from '../pages/NoMatch'
+import Routers from './routers'
+import Home from '../pages/Home'
 
-export default Router = () =>{
-    <Switch>
-        {
-            Routes.map(route =>(
-                <Route {...route} key={route.path} />
-            ))
-        }
-        <Route render={(props) => <NoMatch {...props} />} />
-    </Switch>
-};
+const routerApp = () =>{
+    return(
+        <Routes>
+            <Route element={<Home />} path="/" /> 
+            {/* {Routers.map(route =>(
+                    <Route element={<Home />}  key={route.path} />
+                ))
+            }*/
+            <Route render={(props)=><NoMatch {...props}/>} /> }
+        </Routes>
+    )
+}
+
+export default routerApp;
